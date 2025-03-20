f558967fc79f4dd35a8afb8318489fce2e15ce06
Xinlana1 <julianaoaguilar16@gmail.com>
2025-03-20 18:50:24 -0300

    Closes #3 :sparkles: feat: Get student{id}

--- product-backend/src/main/java/com/abutua/product_backend/controller/StudentController.java
+++ product-backend/src/main/java/com/abutua/product_backend/controller/StudentController.java
@@ -1,14 +1,45 @@
 package com.abutua.product_backend.controller;
-
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.abutua.product_backend.models.Student;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
 public class StudentController {
 
+
+    //list of Students
+    private List<Student> students = Arrays.asList(new Student(1, "student 1", "student1@gmail.com", "number1", 2, 1 ),
+                                                new Student(2, "student 2", "student2@gmail.com", "number2", 1, 2 ),
+                                                new Student(3, "student 3", "student3@gmail.com", "number3", 1, 2 ));
+
+    //endpoint GET students
     @GetMapping("students") //get students
-    public String getStudents() {
-        return "Lista de estudantes";
+    public List<Student> getStudents() {
+        return students;
     }
+
+
+    //endpoint GET students by ID
+     @GetMapping("students/{id}") // responseEnstity trata a resposta
+    public ResponseEntity<Student> getStudent(@PathVariable int id) {
+        Student stud = students.stream() //method to filter the list
+                                .filter(p -> p.getId() == id) //filter the list by ID
+                                .findFirst() //if ID exists, the list will store the student
+                                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Student not found :(")); //exception
+
+        return ResponseEntity.ok(stud); //if not exeption, throw the student!
+
+    }
+
+
 }
